483383bb280bf2cc8bb6151ce77c7d1c353df531
Lakshminarayana K R <lakshminarayana.inbridge@gmail.com>
2017-03-27 12:05:14 +0530

    Model_Offer included

---
+++ app/src/main/java/com/ecash/app/Models/Model_Offer.java
@@ -0,0 +1,18 @@
+package com.ecash.app.Models;
+
+/**
+ * Created by USER on 3/27/2017.
+ */
+
+public class Model_Offer {
+    private int m_id;
+    private String offer;
+
+    public Model_Offer(int m_id,String offer)
+    {
+        this.m_id=m_id;
+        this.offer=offer;
+    }
+
+
+}
